00a78cc9debad3f86b34659551c28565f15643c8
kob112358 <kob112358@gmail.com>
2019-10-05 06:48:41 -0500

    startup

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,11 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CallLog.Properties {
+namespace FarmchemCallLog.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,17 +31,6 @@ namespace CallLog.Properties {
         public string FarmchemCallLogConnectionString {
             get {
                 return ((string)(this["FarmchemCallLogConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KOBPC\\SQLEXPRESS;Initial Catalog=ModifyCallLog;Integrated Security=Tr" +
-            "ue")]
-        public string ModifyCallLogConnectionString {
-            get {
-                return ((string)(this["ModifyCallLogConnectionString"]));
             }
         }
     }
